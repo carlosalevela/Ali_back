abc3b82470c7528a151e087e4e4fc37bb4192e93
Carlos Velasco <carlosvelasco21347@gmail.com>
2025-10-21 16:46:52 -0500

    subiendo url para funcionamiento

--- ali_backend/settings.py
+++ ali_backend/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-8m$ln1spycveqfeoks27lhl#x+t-fd*93empiq(%*e7s%$m75!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','web-production-fada9.up.railway.app']
 
 
 # Application definition
