cb5bd212102d8dda727dc2f6bfc9b3369037ee04
Carlos Velasco <carlosvelasco21347@gmail.com>
2025-10-23 16:52:29 -0500

    set

--- ali_backend/settings.py
+++ ali_backend/settings.py
@@ -111,14 +111,13 @@ CORS_ALLOW_HEADERS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',  
+    'corsheaders.middleware.CorsMiddleware',                  
+    'django.middleware.common.CommonMiddleware',              
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
 ]
 
 ROOT_URLCONF = 'ali_backend.urls'
